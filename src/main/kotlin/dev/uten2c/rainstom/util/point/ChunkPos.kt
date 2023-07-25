f2a7e9ff67f8faf2fc3f22a41bbbd16be5298865
uTen2c <uten2c@gmail.com>
2023-07-25 17:58:48 +0900

    座標系のユーティリティを充実させる

--- src/main/kotlin/dev/uten2c/rainstom/util/point/ChunkPos.kt
+++ src/main/kotlin/dev/uten2c/rainstom/util/point/ChunkPos.kt
@@ -1,9 +1,27 @@
 package dev.uten2c.rainstom.util.point
 
+import net.minestom.server.coordinate.Point
+import net.minestom.server.instance.Chunk
+import net.minestom.server.utils.chunk.ChunkUtils
+
 class ChunkPos(val x: Int, val z: Int) {
     constructor(long: Long) : this(long.toInt(), (long shr 32).toInt())
 
     fun toLong(): Long {
         return x.toLong() and 0xFFFFFFFFL or (z.toLong() and 0xFFFFFFFFL shl 32)
+    }
+
+    companion object {
+        fun fromIndex(index: Long): ChunkPos {
+            return ChunkPos(index.toInt(), (index shr 32).toInt())
+        }
+
+        fun Point.toChunkPos(): ChunkPos {
+            return fromIndex(ChunkUtils.getChunkIndex(this))
+        }
+
+        fun Chunk.toChunkPos(): ChunkPos {
+            return ChunkPos(chunkX, chunkZ)
+        }
     }
 }
--- src/main/kotlin/dev/uten2c/rainstom/util/point/point.kt
+++ src/main/kotlin/dev/uten2c/rainstom/util/point/point.kt
@@ -1,8 +1,39 @@
 package dev.uten2c.rainstom.util.point
 
 import net.minestom.server.coordinate.Point
+import net.minestom.server.coordinate.Pos
 import net.minestom.server.coordinate.Vec
 
 fun Point.asVec(): Vec {
     return Vec(x, y, z)
 }
+
+/**
+ * [Pos]のコンストラクターのシンタックスシュガー
+ */
+fun pos(x: Number, y: Number, z: Number): Pos =
+    Pos(x.toDouble(), y.toDouble(), z.toDouble())
+
+/**
+ * [Pos]のコンストラクターのシンタックスシュガー
+ */
+fun pos(x: Number, y: Number, z: Number, yaw: Number, pitch: Number): Pos =
+    Pos(x.toDouble(), y.toDouble(), z.toDouble(), yaw.toFloat(), pitch.toFloat())
+
+/**
+ * [Pos]のコンストラクターのシンタックスシュガー
+ */
+fun pos(value: Number): Pos =
+    Pos(value.toDouble(), value.toDouble(), value.toDouble())
+
+/**
+ * [Vec]のコンストラクターのシンタックスシュガー
+ */
+fun vec(x: Number, y: Number, z: Number): Vec =
+    Vec(x.toDouble(), y.toDouble(), z.toDouble())
+
+/**
+ * [Vec]のコンストラクターのシンタックスシュガー
+ */
+fun vec(value: Number): Vec =
+    Vec(value.toDouble(), value.toDouble(), value.toDouble())
